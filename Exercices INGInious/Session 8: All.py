dc8ff53ca64604391160fd44b8bb9af9e17510fc
Gaëtan Marchale <46644774+gmarchale@users.noreply.github.com>
2020-01-10 11:36:22 +0100

    Create Session 8: All.py

---
+++ Exercices INGInious/Session 8: All.py
@@ -0,0 +1,9 @@
+# Session 8: Student - init
+
+class Student:
+    def __init__(self, name, surname, birth_date, email):
+        self.name = name
+        self.surname = surname
+        self.birth_date = birth_date
+        self.email = email
+        
